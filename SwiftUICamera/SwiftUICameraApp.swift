0ac53bbe087b770e7b08206c461fc325439c7646
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-22 17:26:45 +0300

    Initial Commit

---
+++ SwiftUICamera/SwiftUICameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraApp.swift
+//  SwiftUICamera
+//
+//  Created by ErRayn on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
