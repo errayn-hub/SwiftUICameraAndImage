224464682d383074113886a4faec908f637fdc58
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-28 20:54:18 +0300

    first commit

---
+++ SwiftUICamera/Preview Content/ImagePicker.swift
@@ -0,0 +1,19 @@
+//
+//  ImagePicker.swift
+//  SwiftUICamera
+//
+//  Created by ErRayn on 28.04.2023.
+//
+
+import Foundation
+import SwiftUI
+ 
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UIImagePickerController
+    // coordinator TODO
+    
+    @Binding var image: UIImage?
+    var sourceType: 
+    
+}
